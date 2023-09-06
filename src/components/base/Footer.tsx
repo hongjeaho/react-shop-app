dbcb96ec748640ec4c582ece60436981899e406e
홍재호 <kansin88@drmamma.co.kr>
2023-09-06 23:40:10 +0900

    Change Fotter

--- src/components/base/Footer.tsx
+++ src/components/base/Footer.tsx
@@ -1,13 +1,21 @@
 import styled from '@emotion/styled/macro'
 import React from 'react'
-import { format } from 'date-fns'
+import { Container } from 'react-bootstrap'
+
+const Footter: React.FC = () => {
+  return (
+    <Footer className="py-5 bg-dark">
+      <Container>
+        <Copyright className="text-center text-white">Copyright &copy; Your Website 2023</Copyright>
+      </Container>
+    </Footer>
+  )
+}
 
 const Footer = styled.footer`
   margin-top: 10px;
 `
 
-const Footter: React.FC = () => {
-  return <Footer>&copy; {format(new Date(), 'yyyy.MM.dd')} twitter</Footer>
-}
+const Copyright = styled.p``
 
 export default Footter
