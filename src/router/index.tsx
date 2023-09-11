2dcc15dd7283dfcce1da862b247defaf8ae69ea0
kansin88 <kansin88@gmail.com>
2023-09-11 14:41:45 +0900

    Remove unused  source file

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { createBrowserRouter, type RouteObject } from 'react-router-dom'
 import BaseLayout from '@/components/layout/BaseLayout'
 import Home from '@/views/Home'
-import info from './info'
-import Login from '@/views/Login'
 import Detail from '@/views/Detail'
 
 const router: RouteObject[] = [
@@ -19,11 +17,6 @@ const router: RouteObject[] = [
         path: '/detail/:sku',
         element: <Detail />,
       },
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      ...info,
     ],
   },
 ]
